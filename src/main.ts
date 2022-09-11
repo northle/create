7d20b218545c3fb719f513592346b32df86ac66c
Doc077 <dom.rajkowski@gmail.com>
2022-09-11 22:02:50 +0200

    Add ZIP extractor

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node --experimental-specifier-resolution=node --no-warnings
 import { createWriteStream } from 'node:fs';
 import { get } from 'node:https';
+import extractZip from 'extract-zip';
 
 process.on('uncaughtException', () => {
   process.exit(1);
@@ -9,11 +10,20 @@ process.on('uncaughtException', () => {
 const repositoryUrl = 'https://github.com/northerjs/norther/archive/refs/heads/main.zip';
 
 get(repositoryUrl, (request) => {
-  const path = createWriteStream(`${process.cwd()}/norther.zip`);
+  const path = `${process.cwd()}/norther.zip`;
+  const file = createWriteStream(path);
 
-  request.pipe(path);
+  request.pipe(file);
 
-  path.on('finish', () => {
-    path.close();
+  file.on('finish', async () => {
+    file.close();
+
+    try {
+      await extractZip(path, {
+        dir: process.cwd(),
+      });
+    } catch (error) {
+      process.exit(1);
+    }
   });
 });
