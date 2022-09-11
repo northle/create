3243e63c6df15f1c976202fe363ec357d1cd4806
Doc077 <dom.rajkowski@gmail.com>
2022-09-11 19:48:17 +0200

    Add repo downloading

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 #!/usr/bin/env node --experimental-specifier-resolution=node --no-warnings
+import { createWriteStream } from 'node:fs';
+import { get } from 'node:https';
 
 process.on('uncaughtException', () => {
   process.exit(1);
+});
+
+const repositoryUrl = 'https://github.com/northerjs/norther/archive/refs/heads/main.zip';
+
+get(repositoryUrl, (request) => {
+  const path = createWriteStream(`${process.cwd()}/norther.zip`);
+
+  request.pipe(path);
+
+  path.on('finish', () => {
+    path.close();
+  });
 });
